0824e3caa31b6360a8ae251c30f2c8c26422580b
Tao Ji <Tao Ji>
2017-03-29 01:51:21 +0800

    Models are built, admin view available

--- vapoursite/fume/admin.py
+++ vapoursite/fume/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Member, Game, Transaction, Publisher, Review, Reward, Genre, Tag, ShoppingCart, Admin
+
+admin.site.register(Member)
+admin.site.register(Game)
+admin.site.register(Transaction)
+admin.site.register(Publisher)
+admin.site.register(Reward)
+admin.site.register(Review)
+admin.site.register(Genre)
+admin.site.register(Tag)
+admin.site.register(ShoppingCart)
+admin.site.register(Admin)
