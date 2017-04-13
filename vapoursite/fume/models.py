1e4ec806cd82fc9a8ef6169f9e15eecf75347e96
gschua <gschua@connect.hku.hk>
2017-04-13 14:30:18 +0800

    Update models.py

--- vapoursite/fume/models.py
+++ vapoursite/fume/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.db.models import Q
 from django.db.models.signals import post_save, pre_save
 from django.dispatch import receiver
 from django.contrib.auth.models import AbstractBaseUser, UserManager, PermissionsMixin
